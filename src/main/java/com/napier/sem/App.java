b0af9c6ea13e98a8884ebd5767a2ee94a8532d33
nicole <nicole.a.jordaan@gmail.com>
2023-02-19 20:08:53 +0000

    Updated App.java

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -2,53 +2,62 @@ package com.napier.sem;
 
 import java.sql.*;
 
-import javax.swing.plaf.basic.BasicToolBarUI;
-
 public class App
 {
+    private Connection con = null;
     public static void main(String[] args)
     {
-       try
-       {
-           // Load Database driver
-           Class.forName("com.mysql.cj.jdbc.Driver");
-       }
-       catch (ClassNotFoundException e)
+        App a = new App();
+
+        // Connect to Database
+        a.connect();
+
+        // Disconnect from Database
+        a.disconnect();
+    }
+
+
+    public void connect()
+    {
+        try
         {
-            System.out.println("Could not load SQL-driver");
+            // Load Database driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println("Could not load SQL Driver");
             System.exit(-1);
         }
 
-       // Connection to the database
-        Connection con = null;
-       int retires = 100;
+        int retries = 10;
+        for (int i = 0; i < retries; ++i)
+        {
+            System.out.println("Connecting to database...");
+            try
+            {
+                // Wait a bit for db to start
+                Thread.sleep(30000);
+                // Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
+                System.out.println("Successfully connected");
+                break;
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println(sqle.getMessage());
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
+        }
+    }
 
-       for (int i = 0; i < retires; ++i)
-       {
-           System.out.println("Connecting to database.....");
-           try
-           {
-               // Wait a bit for db to start
-               Thread.sleep(30000);
-               // Connect to database
-               con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
-               System.out.println("Successfully connected");
-               // Wait for a bit
-               Thread.sleep(10000);
-               // Exit loop
-               break;
-           }
-           catch (SQLException sqle)
-           {
-               System.out.println("Failed to connect to database attempt " + Integer.toString(i));
-               System.out.println(sqle.getMessage());
-           }
-           catch (InterruptedException ie)
-           {
-               System.out.println("Thread interrupted? Should not happen.");
-           }
-       }
 
+    public void disconnect()
+    {
         if (con != null)
         {
             try
@@ -61,8 +70,9 @@ public class App
                 System.out.println("Error closing connection to database");
             }
         }
-
-
     }
+
+
+
 
 }
