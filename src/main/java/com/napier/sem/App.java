fabf2406f2e5bdb567497de214c06f6483eed177
nicole <nicole.a.jordaan@gmail.com>
2023-02-19 22:00:39 +0000

    Created Employee class and Added

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -14,9 +14,13 @@ public class App
 
         // Disconnect from Database
         a.disconnect();
+
     }
 
 
+    /**
+     * Allows connection to a database
+     */
     public void connect()
     {
         try
@@ -55,7 +59,9 @@ public class App
         }
     }
 
-
+    /**
+     * Disconnects from a database
+     */
     public void disconnect()
     {
         if (con != null)
@@ -72,6 +78,44 @@ public class App
         }
     }
 
+    /**
+     * Gets an Employee
+     * @param ID
+     * @return Employee
+     */
+    public Employee getEmployee(int ID)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT emp_no, first_name, last_name "
+                            + "FROM employees "
+                            + "WHERE emp_no = " + ID;
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+            if (rset.next())
+            {
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("emp_no");
+                emp.first_name = rset.getString("first_name");
+                emp.last_name = rset.getString("last_name");
+                return emp;
+            }
+            else
+                return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+            return null;
+        }
+    }
 
 
 
